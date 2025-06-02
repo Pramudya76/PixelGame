0538bb44069c6e6073f45b859e12164d32e17efb
Pramudya76 <pramudyarama76@gmail.com>
2025-06-02 07:57:24 +0700

    Menambah Animasi Player

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveSpeed = 3f;
+    private Animator animator;
+    private float lastMoveX;
+    private float lastMoveY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(moveX * moveSpeed, moveY * moveSpeed);
+
+        animator.SetBool("Walk", moveX != 0);
+        animator.SetBool("WalkTop", Input.GetKey(KeyCode.W));
+        animator.SetBool("WalkDown", Input.GetKey(KeyCode.S));
+
+        if (moveX < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            lastMoveX = moveX;
+            lastMoveY = moveY;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            lastMoveX = moveX;
+            lastMoveY = moveY;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (lastMoveX > 0 || lastMoveX < 0)
+            {
+                animator.SetTrigger("PlayerAttackSide");
+            }
+            else if (lastMoveY < 0)
+            {
+                animator.SetTrigger("AttackPlayer");
+            }
+            else if (lastMoveY > 0)
+            {
+                animator.SetTrigger("PlayerAttackTop");
+            }
+        }
+        
+    }
+}
